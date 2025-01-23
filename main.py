9ae9f3f5bcf37fbde6625eae53fc4076f046cc37
tumpech <tumpechlaszlo@gmail.com>
2025-01-23 15:50:05 +0100

    L4

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from window import Window
+from window import Window, Line, Point
 
 def main():
     win = Window(800,600)
+    line = Line(Point(20,20), Point(300,300))
+    win.draw_line(line,"black")
     win.wait_for_close()
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -21,3 +21,26 @@ class Window():
 
     def close(self):
         self.__running = False
+    
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+class Point():
+    def __init__(self,x ,y ):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self,pointA, pointB):
+        self.pointA = pointA
+        self.pointB = pointB
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.pointA.x,
+            self.pointA.y,
+            self.pointB.x,
+            self.pointB.y,
+            fill=fill_color,
+            width=2
+        )
