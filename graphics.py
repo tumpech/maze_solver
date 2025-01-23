73646b1f5e852eb295147ad990fb5a86c28067d8
tumpech <tumpechlaszlo@gmail.com>
2025-01-23 17:03:27 +0100

    L8

--- graphics.py
+++ graphics.py
@@ -2,28 +2,28 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self,width,height):
-        self.__root = Tk()
-        self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack(fil=BOTH, expand=1)
-        self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self._root = Tk()
+        self._root.title("Maze Solver")
+        self._canvas = Canvas(self._root, bg="white", height=height, width=width)
+        self._canvas.pack(fil=BOTH, expand=1)
+        self._running = False
+        self._root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
+        self._root.update_idletasks()
+        self._root.update()
 
     def wait_for_close(self):
-        self.__running = True
-        while self.__running:
+        self._running = True
+        while self._running:
             self.redraw()
         print("window closed...")
 
     def close(self):
-        self.__running = False
+        self._running = False
     
     def draw_line(self, line, fill_color="black"):
-        line.draw(self.__canvas, fill_color)
+        line.draw(self._canvas, fill_color)
 
 class Point():
     def __init__(self,x ,y ):
--- maze.py
+++ maze.py
@@ -10,38 +10,38 @@ class Maze():
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win=None,
     ):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__num_rows = num_rows
-        self.__num_cols = num_cols
-        self.__cell_size_x = cell_size_x
-        self.__cell_size_y = cell_size_y
-        self.__win = win
-        self.__cells = []
-        self.__create_cells()
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._cells = []
+        self._create_cells()
     
-    def __create_cells(self):
-        self.__cells = [
-            [Cell(self.__win) for y in range(self.__num_rows)]
-            for x in range(self.__num_cols)]
-        for i in range(self.__num_cols):
-            for j in range(self.__num_rows):
+    def _create_cells(self):
+        self._cells = [
+            [Cell(self._win) for y in range(self._num_rows)]
+            for x in range(self._num_cols)]
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
                 self.__draw_cell(i,j)
     
     def __draw_cell(self,i,j):
-        if self.__win == None:
+        if self._win == None:
             return
-        x1 = self.__x1 + i * self.__cell_size_x
-        y1 = self.__y1 + j * self.__cell_size_y
-        x2 = x1 + self.__cell_size_x
-        y2 = y1 + self.__cell_size_y
-        self.__cells[i][j].draw(x1,y1,x2,y2)
-        self.__animate()
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1,y1,x2,y2)
+        self._animate()
     
-    def __animate(self):
-        if self.__win == None:
+    def _animate(self):
+        if self._win == None:
             return
-        self.__win.redraw()
+        self._win.redraw()
         time.sleep(0.05)
