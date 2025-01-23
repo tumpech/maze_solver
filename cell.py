dea7dbbadc82228caeacb0b980c7d7f7ed8dd84e
tumpech <tumpechlaszlo@gmail.com>
2025-01-23 16:39:08 +0100

    L6

--- cell.py
+++ cell.py
@@ -2,9 +2,9 @@ from graphics import Line, Point
 
 class Cell():
     def __init__(self, window):
-        self.x1 = None
-        self.y1 = None
-        self.x2 = None
+        self.__x1 = None
+        self.__y1 = None
+        self.__x2 = None
         self.y2 = None
         self.__window = window
         self.left_wall = True
@@ -14,27 +14,46 @@ class Cell():
 
     
     def draw(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+        if self.__window == None:
+            return
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
         if self.left_wall:
             line = Line(
-                Point(self.x1,self.y1),
-                Point(self.x1,self.y2))
+                Point(self.__x1,self.__y1),
+                Point(self.__x1,self.__y2))
             self.__window.draw_line(line)
         if self.right_wall:
             line = Line(
-                Point(self.x2,self.y1),
-                Point(self.x2,self.y2))
+                Point(self.__x2,self.__y1),
+                Point(self.__x2,self.__y2))
             self.__window.draw_line(line)
         if self.top_wall:
             line = Line(
-                Point(self.x1,self.y1),
-                Point(self.x2,self.y1))
+                Point(self.__x1,self.__y1),
+                Point(self.__x2,self.__y1))
             self.__window.draw_line(line)
         if self.bottom_wall:
             line = Line(
-                Point(self.x1,self.y2),
-                Point(self.x2,self.y2))
+                Point(self.__x1,self.__y2),
+                Point(self.__x2,self.__y2))
             self.__window.draw_line(line)
+    
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            color="grey"
+        else:
+            color="red"
+        half_length = abs(self.__x2 - self.__x1) // 2
+        x_center = half_length + self.__x1
+        y_center = half_length + self.__y1
+        startPoint = Point(x_center, y_center)
+
+        half_length2 = abs(to_cell.__x2 - to_cell.__x1) // 2
+        x_center2 = half_length2 + to_cell.__x1
+        y_center2 = half_length2 + to_cell.__y1
+        endPoint = Point(x_center2,y_center2)
+
+        self.__window.draw_line(Line(startPoint,endPoint),color)
--- main.py
+++ main.py
@@ -4,21 +4,26 @@ from cell import Cell
 def main():
     win = Window(800,600)
     
-    c = Cell(win)
-    c.left_wall = False
-    c.draw(20,20,170,170)
+    c1 = Cell(win)
+    c1.left_wall = False
+    c1.draw(20,20,170,170)
 
-    c = Cell(win)
-    c.right_wall = False
-    c.draw(180,20,320,170)
+    c2 = Cell(win)
+    c2.right_wall = False
+    c2.draw(180,20,320,170)
 
-    c = Cell(win)
-    c.top_wall = False
-    c.draw(20,180,170,320)
+    c3 = Cell(win)
+    c3.top_wall = False
+    c3.draw(20,180,170,320)
 
-    c = Cell(win)
-    c.bottom_wall = False
-    c.draw(180,180,320,320)
+    c4 = Cell(win)
+    c4.bottom_wall = False
+    c4.draw(180,180,320,320)
+
+    c1.draw_move(c2)
+    c2.draw_move(c4)
+    c4.draw_move(c2,undo=True)
+
 
     line = Line(Point(20,20), Point(300,300))
     win.draw_line(line,"black")
